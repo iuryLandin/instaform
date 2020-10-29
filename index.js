e58d1f6420154b8aba853e1845852fb0a25baca0
Iury Landin <iurylandin@gmail.com>
2020-10-29 11:08:49 -0300

    notficacao

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ app.get('/', function(req, res) {
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
+    fetch("http://cdrtechnology.com.br:5000/chat/sendmessage/556392413975", {
+            "method": "POST",
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "body": {
+                "message": JSON.stringify(req.body)
+            }
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
     if (
         req.query['hub.mode'] == 'subscribe' &&
         req.query['hub.verify_token'] == token
