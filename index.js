d10dfb2e31fd18d6f2fe6b9081e303fabbf5fa2d
Iury Landin <iurylandin@gmail.com>
2020-10-29 10:01:13 -0300

    change endpoints

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 
-var controller = require('./facebook.controller');
-
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
@@ -12,12 +10,46 @@ app.use(bodyParser.json());
 var token = process.env.TOKEN || 'token102030';
 var received_updates = [];
 
+ar received_updates = [];
+
 app.get('/', function(req, res) {
     console.log(req);
     res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
-app.get('/index', controller.index);
-app.post('/webhook', controller.webhook);
+app.get(['/facebook', '/instagram'], function(req, res) {
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == token
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.post('/facebook', function(req, res) {
+    console.log('Facebook request body:', req.body);
+
+    if (!req.isXHubValid()) {
+        console.log('Warning - request header X-Hub-Signature not present or invalid');
+        res.sendStatus(401);
+        return;
+    }
+
+    console.log('request header X-Hub-Signature validated');
+    // Process the Facebook updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+    console.log('Instagram request body:');
+    console.log(req.body);
+    // Process the Instagram updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
 
 app.listen();
