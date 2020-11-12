f84ccff9ae28ac69a51b4909bb35ccf78420de72
Iury Landin <iurylandin@gmail.com>
2020-11-12 16:55:41 -0300

    webhook fb

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-const express = require('express');
-const API = require('./leads')
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
+var bodyParser = require('body-parser');
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+app.use(bodyParser.json());
+
+var token = process.env.TOKEN || 'token102030';
+var received_updates = [];
 
 app.get('/', function(req, res) {
-    API({
-        token: req.body.token,
-        formId: req.body.formId,
-    }, function(resultado) {
-        res.send(resultado);
-        console.log(resultado)
-    });
-
+    console.log(req);
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
+
+app.get(['/facebook', '/instagram'], function(req, res) {
+    if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == token
+    ) {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+app.post('/facebook', function(req, res) {
+    console.log('Facebook request body:', req.body);
+
+
+
+
+
+    // Process the Facebook updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+    console.log('Instagram request body:');
+    console.log(req.body);
+    // Process the Instagram updates here
+    received_updates.unshift(req.body);
+    res.sendStatus(200);
+});
+
+app.listen();
