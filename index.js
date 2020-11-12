7bf1cd428981fe1c2ea4a1b3fd62316260f92424
Iury Landin <iurylandin@gmail.com>
2020-11-12 16:01:26 -0300

    fix bug besta

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
- var app = express();
- const API = require('./leads')
+const express = require('express');
+const API = require('./leads')
 
- app.set('port', (process.env.PORT || 5000));
- app.listen(app.get('port'));
+var app = express();
 
- app.use(
-     express.urlencoded({
-         extended: true
-     })
- )
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
 
- app.get('/', function(req, res) {
-     API({
-         token: req.body.token,
-         formId: req.body.formId,
-     }, function(resultado) {
-         res.send(resultado);
-         console.log(resultado)
-     });
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
 
- });
+app.get('/', function(req, res) {
+    API({
+        token: req.body.token,
+        formId: req.body.formId,
+    }, function(resultado) {
+        res.send(resultado);
+        console.log(resultado)
+    });
+
+});
