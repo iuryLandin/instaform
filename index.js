c65dec5106f3ed87070700af5dbcc70e368ca300
Iury Landin <iurylandin@gmail.com>
2020-11-12 19:04:45 -0300

    webhook funcionando

--- index.js
+++ index.js
@@ -37,26 +37,20 @@ app.get('/facebook', function(req, res) {
 });
 
 app.post('/facebook', async function(req, res) {
-    console.log(JSON.stringify(req.body));
 
-
-    console.log("\n\n\n");
-    console.log(req.body.entry[0].changes[0].value.leadgen_id);
-    console.log("\n\n\n");
 
     // token de produção do app
     var app_token = '273565973982869|mA9Tj_TrYF_RYsGtYmygHRZDKj4';
 
     //declara variavel com id do lead recebido
-    // var leadgen_id = lead_data.changes[0].value.leadgen_id;
-
-    var lead_data = '0';
+    var leadgen_id = req.body.entry[0].changes[0].value.leadgen_id;
 
     //faz uma requisição no facebook para pegar os dados do formulario do lead
     await fetch(`"https://graph.facebook.com/v8.0/${leadgen_id}?access_token=${app_token}`)
         .then(resultado => {
             console.log(resultado.data)
         });
+
 
     // Process the Facebook updates here
     received_updates.unshift(req.body);
